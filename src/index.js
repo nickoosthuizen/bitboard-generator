ee7b5b4621e1e616edc9e57055f823946b9ce6b4
Nick Oosthuizen <nickoosthuizen@g.ucla.edu>
2021-08-01 20:54:51 -0500

    Initial, built off the react tutorial

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(64).fill(0),
+      number: null
+    };
+  }
+
+  handleClick(i) {
+    var squares = this.state.squares.slice();
+    squares[i] = this.state.squares[i] === 0 ? 1 : 0;
+    this.setState({
+      squares: squares,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  calculateNumber() {
+    const hexString = parseInt(this.state.squares.join(""), 2).toString(16).padStart(16, 0);
+    this.setState({
+      squares: Array(64).fill(0),
+      number: hexString,
+    });
+  }
+
+  render() {
+    var grid = [];
+    for (var i = 0; i < 8; i++) {
+      grid.push([])
+      for (var j = 0; j < 8; j++) {
+        grid[i].push(i * 8 + j);
+      }
+    }
+    return (
+      <div>
+        <div>
+          {grid.map(row => {
+            return (
+              <div className="board-row">
+                {row.map(i =>
+                  <Square 
+                      key={i} 
+                      value={this.state.squares[i]} 
+                      onClick={() => this.handleClick(i)}
+                  />
+                )}
+              </div>
+            )
+          })}
+        </div>
+        <div className="status">Represented Hex Value: {this.state.number}</div>
+        <button onClick={() => this.calculateNumber()}>Calculate</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('root')
+);
